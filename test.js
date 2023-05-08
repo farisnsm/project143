6f0879b66b3e49c3919638a589669572a5356f14
Faris <faris@timeaccessworld.com>
2023-05-08 20:43:28 +0800

    first commit

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-let arr = [{a:1,b:"x"},{a:2,b:"y"},{a:3,b:"x"},{a:4,b:"e"}]
-arr = arr.sort((a,b) => (b.b == "x") - (a.b=="x")); // b - a for reverse sort
+let arr = [{a:1,b:"x"},{a:2,b:"y"},{a:3,b:"x"},{a:4,b:"e"},{a:5,b:"y"}]
+arr = arr.sort((a,b) => (a.b > b.b) ? 1 : ((b.b > a.b) ? -1 : 0)).sort((a,b) => (b.b == "x") - (a.b=="x")); // b - a for reverse sort
 console.log(arr)
